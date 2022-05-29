b39126cdd7867de0fa67a98321f81bb64e434651
Steven <stevendawn98@gmail.com>
2022-05-28 20:25:03 -0400

    commit tree 0475c76b0e506d215fb64247ccfb82ee4a2509c9

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+const textToChange = document.getElementById('text')
+
+document.addEventListener("DOMContentLoaded",() => {
+   textToChange.textContent = 'This is really cool!'
+})
